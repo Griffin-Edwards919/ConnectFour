f0d9ff36aff208c7e3b2b7249ec21249bbd496c7
21edwards <21edwards@da.org>
2021-03-05 19:01:41 -0500

    Fixed new file

--- GriffinAI.py
+++ GriffinAI.py
@@ -3,6 +3,8 @@ import time, math, numpy
 STARTING_DEPTH = 1
 checked_board_states = []
 max_depth = STARTING_DEPTH
+ROW_COUNT = 6
+COLUMN_COUNT = 7
 
 def get_move(board, piece):
     global max_depth, checked_board_states
@@ -137,3 +139,46 @@ def score_board(board, piece):
                 elif ocounter == 3: score -= 5000
                 elif ocounter == 2: score -= 10
     return score
+
+def board_is_full(board):
+    for row in board:
+        for column in row:
+            if column == 0: return False
+    return True
+
+
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
+
+def is_valid_location(board, col):
+    return board[ROW_COUNT - 1][col] == 0
+
+def get_next_open_row(board, col):
+    for r in range(ROW_COUNT):
+        if board[r][col] == 0:
+            return r
+
+def winning_move(board, piece):
+    # Check horizontal locations for win
+    for c in range(COLUMN_COUNT - 3):
+        for r in range(ROW_COUNT):
+            if (board[r][c] == piece) and (board[r][c + 1] == piece) and (board[r][c + 2] == piece) and (board[r][c + 3] == piece):
+                return True
+
+    # Check vertical locations for win
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT - 3):
+            if (board[r][c] == piece) and (board[r + 1][c] == piece) and (board[r + 2][c] == piece) and (board[r + 3][c] == piece):
+                return True
+
+    # Check positively sloped diaganols
+    for c in range(COLUMN_COUNT - 3):
+        for r in range(ROW_COUNT - 3):
+            if (board[r][c] == piece) and (board[r + 1][c + 1] == piece) and (board[r + 2][c + 2] == piece) and (board[r + 3][c + 3] == piece):
+                return True
+
+    # Check negatively sloped diaganols
+    for c in range(COLUMN_COUNT - 3):
+        for r in range(3, ROW_COUNT):
+            if (board[r][c] == piece) and (board[r - 1][c + 1] == piece) and (board[r - 2][c + 2] == piece) and (board[r - 3][c + 3] == piece):
+                return True
